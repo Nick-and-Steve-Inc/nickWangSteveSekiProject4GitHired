f3a00d562048f210674cf970c9ca3855c95bb4d4
tseki88 <teruhisaseki@gmail.com>
2019-11-22 11:05:05 -0500

    refactored scripts

--- script/script.js
+++ script/script.js
@@ -24,21 +24,24 @@ gitHiredApp.teleCityReusableApiCall = (cityName, infoType) => {
     method: "GET"
   });
 };
-  
-gitHiredApp.teleCityImageAjaxCall = (cityName) => {
 
+
+gitHiredApp.cityImageUrlObject = {};
+
+gitHiredApp.teleCityImageAjaxCall = (cityName) => {
   let selectCityImageContainer = $(`.${cityName} .imageContainer`);
-  // City Api
   gitHiredApp.teleCityReusableApiCall(cityName, "images").then(res => {
     const renderHtml = `<img src=${
-      res.photos[i].image.mobile
+      res.photos[0].image.mobile
     } alt="This is a photo of ${cityName.replace("-", " ")}" />`;
+    
+    gitHiredApp.cityImageUrlObject[cityName] = renderHtml;
     selectCityImageContainer.append(renderHtml);
   });
 };
 
 
-const cityImageUrlArray = [];
+
 
 gitHiredApp.githubJobsReusableApiCall = cityName => {
   // Github jobs API
@@ -75,20 +78,15 @@ gitHiredApp.githubJobsAjaxCall = cityName => {
 };
 
 gitHiredApp.gitTeleportCityDetails = cityName => {
+// city details call for second page
+};
 
-}
-
-gitHiredApp.returnSelectedCityValue = () => {
-  const citySelected = $(".citySelect");
+gitHiredApp.handleOnChangeJobDetails = (selectedSingleCity) => {
   const jobListingContainer = $(".jobListingsContainer");
 
-  citySelected.on("change", e => {
-    jobListingContainer.html("");
-    let selectedSingleCity = e.target.value;
-
-    gitHiredApp.githubJobsReusableApiCall(selectedSingleCity).then(res => {
-      for (let i = 0; i < 6; i++) {
-        const renderHtml = `
+  gitHiredApp.githubJobsReusableApiCall(selectedSingleCity).then(res => {
+    for (let i = 0; i < 6; i++) {
+      const renderHtml = `
         <div class="singleJobPost"> 
           <h3>${res[i].title}</h3>
           <p class="jobType">${res[i].type}</p>
@@ -96,9 +94,25 @@ gitHiredApp.returnSelectedCityValue = () => {
           <p class="createdAt">Company: ${res[i].created_at}</p>
           <p><a href=${res[i].url} target="_blank">Apply Here</a></p>
         <div/>`;
-        jobListingContainer.append(renderHtml);
-      };
-    });
+      jobListingContainer.append(renderHtml);
+    };
+  });
+}
+
+gitHiredApp.handleOnChangeCityDetails = (selectedSingleCity) => {
+  
+};
+
+
+gitHiredApp.returnSelectedCityValue = () => {
+  const citySelected = $(".citySelect");
+
+  
+  citySelected.on("change", e => {
+    jobListingContainer.html("");
+    let selectedSingleCity = e.target.value;
+    gitHiredApp.handleOnChangeJobDetails(selectedSingleCity);
+
   });
 };
 
@@ -107,6 +121,7 @@ gitHiredApp.populateWithImagesAndJobs = () => {
     gitHiredApp.teleCityImageAjaxCall(city);
     gitHiredApp.githubJobsAjaxCall(city);
   });
+  console.log(gitHiredApp.cityImageUrlObject);
 };
 
 $(document).ready(function() {
